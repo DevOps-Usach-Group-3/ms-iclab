aa03c358d829ce94d1fd24a98695c3ea73f9a36d
Ellery Fahrenkrog <efahren@gmail.com>
2022-02-06 18:46:44 -0300

    fix automatizacion

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -24,7 +24,7 @@ public class RestData {
 
 	@GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message){
-		LOGGER.log(Level.INFO, "Proceso exitoso de prueba");
+		LOGGER.log(Level.INFO, "Proceso exitoso de prueba ");
 		Pais response = new Pais();
 		response.setMensaje("Mensaje Recibido: " + message);
 		return response;
